9ade3d7d74a213ef724aac63b6fba5f335e59522
git-shad0w <aboodshadow@outlook.com>
2020-12-16 21:21:48 +0400

    Added background video (looks cooler)

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,10 +4,29 @@ import React from "react";
 import Nav from "../Navbar/Nav";
 import Overlay from "./Overlay";
 import Info from "./Info";
-import background from "../Image/background2.png";
+import Login from "./Login";
+import vid1 from "../video/vid2.mp4";
 
 const Main = () => (
   <section css={styles} className="main" id="home">
+    <video
+      autoPlay
+      loop
+      muted
+      style={{
+        position: "absolute",
+        width: "100%",
+        left: "50%",
+        top: "50%",
+        height: "100%",
+        objectFit: "cover",
+        transform: "translate(-50%, -50%)",
+        zIndex: "-1",
+      }}
+    >
+      <source src={vid1} type="video/mp4" />
+    </video>
+    <Login />
     <Overlay />
     <Nav />
     <Info />
@@ -17,7 +36,6 @@ const Main = () => (
 const styles = css`
   width: 100%;
   height: 100vh;
-  background: url("${background}") no-repeat center/cover;
 `;
 
 export default Main;
