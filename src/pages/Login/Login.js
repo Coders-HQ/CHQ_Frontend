b27766d749bc4becdf74cf52b8fc99432d6fcd8c
git-shad0w <aboodshadow@outlook.com>
2020-12-20 15:31:39 +0400

    Added Login, Register pages

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div className="Login">
+      <span>Test - Login</span>
+    </div>
+  );
+};
+export default Login;
---
+++ src/pages/Register/Register.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Register = () => {
+  return (
+    <div className="Register">
+      <span>Test - Register</span>
+    </div>
+  );
+};
+export default Register;
