bcbeab0f31a43fd9ef84f35397aa37fb9305c2ee
git-shad0w <aboodshadow@outlook.com>
2020-12-20 15:03:19 +0400

    Added Routes (from hamad's branch)

---
+++ src/pages/Cause/Cause.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {ReactComponent as SVG1} from './1.svg';
+import {ReactComponent as SVG2} from './2.svg';
+
+
+const Cause = () => {
+  return (
+    <div className="Incentive">
+      <SVG1 />
+      <SVG2 />
+    </div>
+  );
+}
+export default Cause;
---
+++ src/pages/Incentive/Incentive.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {ReactComponent as SVG1} from './1.svg';
+import {ReactComponent as SVG2} from './2.svg';
+import {ReactComponent as SVG3} from './3.svg';
+import {ReactComponent as SVG4} from './4.svg';
+import {ReactComponent as SVG5} from './5.svg';
+import {ReactComponent as SVG6} from './6.svg';
+import {ReactComponent as SVG7} from './7.svg';
+
+
+const Incentive = () => {
+  return (
+    <div className="Incentive">
+      <SVG1 />
+      <SVG2 />
+      <SVG3 />
+      <SVG4 />
+      <SVG5 />
+      <SVG6 />
+      <SVG7 />
+    </div>
+  );
+}
+export default Incentive;
