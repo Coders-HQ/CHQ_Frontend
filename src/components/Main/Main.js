694a8d1387852dd0cf82505993c30755e2e660a2
git-shad0w <aboodshadow@outlook.com>
2020-12-20 20:43:35 +0400

    Added Switch to Main.js

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -9,9 +9,11 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const Main = () => (
   <Router>
-    <Route path="/login" component={Login} />
-    <Route path="/register" component={Register} />
-    <Route path="/" component={Home} />
+    <Switch>
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
+      <Route path="/" component={Home} />
+    </Switch>
   </Router>
 );
 
