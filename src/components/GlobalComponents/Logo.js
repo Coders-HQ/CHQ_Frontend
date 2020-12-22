2a503deea447cfc048e69d6c4f7e1f49b6b5b412
git-shad0w <aboodshadow@outlook.com>
2020-12-22 21:25:15 +0400

    Changed width in Logo.js

--- src/components/GlobalComponents/Logo.js
+++ src/components/GlobalComponents/Logo.js
@@ -4,7 +4,7 @@ import ReactLogo from "../../components/Image/chq_logos/png/dark_notext.png";
 const Logo = () => {
   return (
     <div className="App">
-      <img src={ReactLogo} alt="Coders HQ Logo" />
+      <img src={ReactLogo} alt="Coders HQ Logo" width="350px" />
     </div>
   );
 };
