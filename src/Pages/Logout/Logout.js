4f33202c7fa8cf3f877d334508c78e94488cb55a
git-shad0w <aboodshadow@outlook.com>
2020-12-29 21:39:16 +0400

    Logout API added

---
+++ src/Pages/Logout/Logout.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Logout = () => {
+  return (
+    <div>
+      <h1>
+        Welcome <span className="user__name">Abdulrahman</span>
+        <button className="logout__button">Logout</button>
+      </h1>
+    </div>
+  );
+};
+
+export default Logout;
