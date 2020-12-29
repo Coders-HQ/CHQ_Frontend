0f4717c66e741d9b73f8c475ceff6d3dd252f573
git-shad0w <aboodshadow@outlook.com>
2020-12-29 21:39:23 +0400

    Register API added

--- src/pages/Register/Register.js
+++ src/pages/Register/Register.js
@@ -1,7 +1,207 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Error from "./Error";
+import {
+  Button,
+  CssBaseline,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  Link,
+  Grid,
+} from "@material-ui/core";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import { red } from "@material-ui/core/colors";
+import Logo from "../../Components/GlobalComponents/Logo";
+import { useDispatch } from "react-redux";
+import { login } from "../../Features/userSlice";
+import axios from "axios";
 
 const Register = () => {
-  return <div>Register</div>;
+  const classes = useStyles();
+
+  const [username, setUsername] = useState("");
+  const [pass, setPass] = useState("");
+  const [confirmpass, setConfirmPass] = useState("");
+  const [email, setEmail] = useState("");
+  const [registerError, setRegisterError] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    axios
+      .post("https://coders-hq.herokuapp.com/auth/register/", {
+        username: username,
+        password1: pass,
+        password2: confirmpass,
+        email: email,
+      })
+      .then((res) => console.log(res))
+      .catch((e) => setRegisterError(true));
+  };
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Logo />
+        <Typography component="h1" variant="h5">
+          Register
+        </Typography>
+        <Error status={registerError} />
+        <form
+          className={classes.form}
+          noValidate
+          onSubmit={(e) => handleSubmit(e)}
+        >
+          <TextField
+            className={classes.field}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="username"
+            label="Username"
+            name="username"
+            autoFocus
+            autoComplete="off"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <TextField
+            className={classes.field}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            value={pass}
+            onChange={(e) => setPass(e.target.value)}
+          />
+
+          <TextField
+            className={classes.field}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="confirm-password"
+            label="Confirm Password"
+            type="password"
+            id="confirm-password"
+            autoComplete="current-password"
+            value={confirmpass}
+            onChange={(e) => setConfirmPass(e.target.value)}
+          />
+
+          <TextField
+            className={classes.field}
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="email"
+            label="Email Address"
+            type="email"
+            id="email"
+            autoComplete="off"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="secondary"
+            className={classes.submit}
+          >
+            Register
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link
+                href="/"
+                variant="body2"
+                color="red"
+                className={classes.link}
+              >
+                Back to home page
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link
+                href="/login"
+                variant="body2"
+                color="red"
+                className={classes.link}
+              >
+                {"Already have an account? Login"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  );
 };
+
+// Material-UI Styling
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    color: theme.palette.getContrastText(red[500]),
+    backgroundColor: red[500],
+    "&:hover": {
+      backgroundColor: red[700],
+    },
+    margin: theme.spacing(3, 0, 2),
+  },
+  link: {
+    color: "red",
+    "&:hover, &:focus": {
+      color: "darkred",
+    },
+    "&:active": {
+      color: "red",
+    },
+  },
+  field: {
+    "& label.Mui-focused": {
+      color: "red",
+    },
+    "& .MuiInput-underline:after": {
+      borderBottomColor: "black",
+    },
+    "& .MuiOutlinedInput-root": {
+      "& fieldset": {
+        borderColor: "black",
+      },
+      "&:hover fieldset": {
+        borderColor: "darkred",
+      },
+      "&.Mui-focused fieldset": {
+        borderColor: "red",
+      },
+    },
+  },
+}));
 
 export default Register;
