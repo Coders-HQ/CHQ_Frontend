dac2cdde623279b669bc128aeda92d0ebcebea65
git-shad0w <aboodshadow@outlook.com>
2020-12-31 00:38:37 +0400

    Loading Effect for API

---
+++ src/Components/GlobalComponents/Loading.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { css } from "@emotion/core";
+import FadeLoader from "react-spinners/FadeLoader";
+import Fade from "@material-ui/core/Fade";
+
+const Loading = ({ loading }) => {
+  // Can be a string as well. Need to ensure each key-value pair ends with ;
+
+  const override = css`
+    margin: 0;
+    position: absolute;
+    top: 50%;
+    left: 51%;
+    transform: translate(-50%, -50%);
+  `;
+
+  return (
+    <Fade in={loading}>
+      <div className="loading">
+        <FadeLoader
+          css={override}
+          size={50}
+          color={"rgb(255,0,0)"}
+          loading={loading}
+        />
+      </div>
+    </Fade>
+  );
+};
+
+export default Loading;
