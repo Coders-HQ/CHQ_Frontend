745fa21ec042b051e13bbd792e81950d3340e86e
git-shad0w <aboodshadow@outlook.com>
2021-01-01 16:24:30 +0400

    New homepage design

--- src/Pages/Home/Navbar/LinksContainer.js
+++ src/Pages/Home/Navbar/LinksContainer.js
@@ -4,24 +4,23 @@ import React from "react";
 import Link from "./Link";
 import Button from "../../../Components/GlobalComponents/Button";
 import Github from "./Github";
-import { useSelector, useDispatch } from "react-redux";
-import { selectUser, logout } from "../../../Features/userSlice";
 
 const LinksContainer = ({ hidden }) => {
-  const user = useSelector(selectUser);
-  const dispatch = useDispatch();
+  const user = false;
 
   const handleLogout = (e) => {
     e.preventDefault();
-    dispatch(logout());
+    //dispatch(logout());
   };
 
   return (
     <div className={(hidden ? "hidden" : "") + " linksContainer"}>
-      <Link name="HOME" linkTo="/" />
-      <Link name="INCENTIVE" linkTo="/incentive" />
-      <Link name="OUR CAUSE" linkTo="/cause" />
-      <Link name="OUR GROUPS" linkTo="/groups" />
+      <div className="mid">
+        <Link name="HOME" linkTo="/" />
+        <Link name="INCENTIVE" linkTo="/incentive" />
+        <Link name="OUR CAUSE" linkTo="/cause" />
+        <Link name="OUR GROUPS" linkTo="/groups" />
+      </div>
       {user ? "" : <Button text="JOIN NOW" linkTo="/register" />}
       {user ? (
         <Button text="LOGOUT" onClick={(e) => handleLogout(e)} />
--- src/pages/Home/Info.js
+++ src/pages/Home/Info.js
@@ -2,15 +2,32 @@
 
 import React from "react";
 import Button from "../../Components/GlobalComponents/Button";
+import TextLoop from "react-text-loop";
 
+const user = "Abdulrahman"; // Represents user's first name
+const Auth = true;
 const Info = () => (
   <div className="info">
-    <h1>
-      <span>SHAKING</span> THE <span>WORLD</span> USING <span>CODE</span>
+    <h1 className="welcomeMessage">
+      {Auth ? "Welcome back, " : "Hello there!"}{" "}
+      <span className="firstname">{user}</span>
     </h1>
-    <p>
-      <Button center text="WHAT ARE WE?"></Button>
-    </p>
+    <h1 className="maintext">
+      <TextLoop
+        className="purple"
+        children={[
+          "Designing",
+          "Creating",
+          "Engineering",
+          "Envisioning",
+          "Planning",
+          "Picturing",
+          "Making",
+        ]}
+      ></TextLoop>{" "}
+      the future using code.
+      <h1 className="subtext">Coders HQ</h1>
+    </h1>
   </div>
 );
 
