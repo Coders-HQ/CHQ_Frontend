66feb7aa1aad8cf1d9489c3d5f5dcf5460685fde
git-shad0w <aboodshadow@outlook.com>
2021-01-05 17:32:20 +0400

    Added a TODO list (use wayou.vscode-todo-highligh)

--- src/App.js
+++ src/App.js
@@ -1,3 +1,13 @@
+/**
+ * TODO:
+ * - Welcome/Home Page (React-Scroll & Requires new Layout)
+ * - Profile/Dashboard Page (Would show functions a user could do including adding an API)
+ * - Search Component (To be added to all pages as a quick results version & should be an advanced search on its own page)
+ * - Challenges Page/Component
+ * - Thrillacons Page/Component
+ * - CHQ CV Page/Component (Should be part of the profile)
+ */
+
 import React from "react";
 import "./Styles/app.scss";
 import Main from "./Components/Main/Main";
