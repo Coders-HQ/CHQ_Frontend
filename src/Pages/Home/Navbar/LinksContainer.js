e1c402973272663d8d88abecfb74f22ddef5e74e
git-shad0w <aboodshadow@outlook.com>
2021-01-05 17:28:34 +0400

    Added useEffect to fix after page load auth

--- src/Pages/Home/Navbar/LinksContainer.js
+++ src/Pages/Home/Navbar/LinksContainer.js
@@ -1,12 +1,23 @@
 /** @jsxImportSource @emotion/core */
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Link from "./Link";
 import Button from "../../../Components/GlobalComponents/Button";
 import Github from "./Github";
 import { logout, isAuth } from "../../../Services/auth.service";
 
 const LinksContainer = ({ hidden }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    if (isAuth()) {
+      setIsAuthenticated(true);
+    } else {
+      setIsAuthenticated(false);
+    }
+    console.log("hi from useeffect");
+  }, [isAuthenticated]);
+
   return (
     <div className={(hidden ? "hidden" : "") + " linksContainer"}>
       <div className="mid">
@@ -17,7 +28,7 @@ const LinksContainer = ({ hidden }) => {
       </div>
       {
         // If the user is authenticated it will show the register and login otherwise only logout button
-        isAuth() ? (
+        isAuthenticated ? (
           <Button text="LOGOUT" linkTo="/logout" />
         ) : (
           <div>
