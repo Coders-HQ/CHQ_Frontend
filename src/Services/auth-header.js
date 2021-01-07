57654cf0b19202e37427ed90a3cd3980120577b4
git-shad0w <aboodshadow@outlook.com>
2021-01-07 14:33:34 +0400

    Created new Authentication Function

--- src/Services/auth-header.js
+++ src/Services/auth-header.js
@@ -1,9 +1,28 @@
-export default function authHeader() {
-  const accessToken = JSON.parse(localStorage.getItem("accessToken"));
+import axios from "axios";
 
-  if (accessToken) {
-    return { Authorization: "Token " + accessToken };
-  } else {
-    return {};
-  }
-}
+const authHeader = () => {
+  const accessToken = localStorage.getItem("accessToken");
+  const userData = localStorage.getItem("userData");
+  const API_URL = "https://coders-hq.herokuapp.com/auth/";
+  const config = {
+    method: "get",
+    url: API_URL + "user/",
+    headers: {
+      Authorization: "Token " + accessToken,
+    },
+  };
+
+  return axios(config)
+    .then(function (response) {
+      if (response.status !== 401 || response.status !== 400) {
+        return true;
+      } else return false;
+    })
+    .catch(function (error) {
+      if (error.response.status === 200) {
+        return true;
+      } else return false;
+    });
+};
+
+export default authHeader;
