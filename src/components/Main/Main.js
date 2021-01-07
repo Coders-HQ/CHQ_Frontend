9dfef06404c02886f9de5a2533691840c73ca582
git-shad0w <aboodshadow@outlook.com>
2021-01-07 14:33:26 +0400

    Fixed Authentication Bug

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -6,16 +6,36 @@ import Home from "../../Pages/Home/Home";
 import Login from "../../Pages/Login/Login";
 import Register from "../../Pages/Register/Register";
 import Logout from "../../Pages/Logout/Logout";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Dashboard from "../../Pages/Dashboard/Dashboard";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import authHeader from "../../Services/auth-header";
+console.log(authHeader());
 
 const Main = () => {
+  console.log(authHeader().then());
   return (
     <Router>
       <Switch>
-        <Route path="/login" component={Login} />
-        <Route path="/register" component={Register} />
-        <Route path="/logout" component={Logout} />
-        <Route path="/" component={Home} />
+        <Route exact path="/dashboard">
+          {authHeader() ? <Dashboard /> : <Redirect to="/login" />}
+        </Route>
+        <Route exact path="/login">
+          {authHeader() ? <Redirect to="/dashboard" /> : <Login />}
+        </Route>
+        <Route exact path="/register">
+          {authHeader() ? <Redirect to="/dashboard" /> : <Register />}
+        </Route>
+        <Route exact path="/logout">
+          {authHeader() ? <Logout /> : <Redirect to="/dashboard" />}
+        </Route>
+        <Route exact path="/">
+          <Home />
+        </Route>
       </Switch>
     </Router>
   );
