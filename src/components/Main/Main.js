8c097e55def02cb07973a76a86dce22b5eeca359
git-shad0w <aboodshadow@outlook.com>
2021-01-11 13:17:19 +0400

    Added Authenticated Routes

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,44 +1,101 @@
 /** @jsxImportSource @emotion/core */
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import Home from "../../Pages/Home/Home";
 import Login from "../../Pages/Login/Login";
 import Register from "../../Pages/Register/Register";
 import Logout from "../../Pages/Logout/Logout";
 import Dashboard from "../../Pages/Dashboard/Dashboard";
+import NotFound from "../../Pages/NotFound/NotFound";
+import Loading from "../GlobalComponents/Loading";
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Redirect,
 } from "react-router-dom";
-import authHeader from "../../Services/auth-header";
-console.log(authHeader());
+import { authHeader } from "../../Services/auth-header";
 
-const Main = () => {
-  console.log(authHeader().then());
+const Main = ({ props }) => {
+  const [isAuthenticated, userHasAuthenticated] = useState(false);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    onLoad();
+  }, []);
+
+  async function onLoad() {
+    try {
+      await authHeader();
+      userHasAuthenticated(true);
+      setLoading(false);
+      console.log("User is Authenticated");
+    } catch (e) {
+      userHasAuthenticated(false);
+      setLoading(false);
+      console.log("User not Authenticated");
+    }
+  }
   return (
     <Router>
       <Switch>
         <Route exact path="/dashboard">
-          {authHeader() ? <Dashboard /> : <Redirect to="/login" />}
+          {isLoading ? (
+            <Loading loading={isLoading} />
+          ) : isAuthenticated ? (
+            <Dashboard />
+          ) : (
+            <Redirect to="/login" />
+          )}
         </Route>
         <Route exact path="/login">
-          {authHeader() ? <Redirect to="/dashboard" /> : <Login />}
+          {isLoading ? (
+            <Loading loading={isLoading} />
+          ) : isAuthenticated ? (
+            <Redirect to="/dashboard" />
+          ) : (
+            <Login />
+          )}
         </Route>
         <Route exact path="/register">
-          {authHeader() ? <Redirect to="/dashboard" /> : <Register />}
+          {isLoading ? (
+            <Loading loading={isLoading} />
+          ) : isAuthenticated ? (
+            <Redirect to="/dashboard" />
+          ) : (
+            <Register />
+          )}
         </Route>
         <Route exact path="/logout">
-          {authHeader() ? <Logout /> : <Redirect to="/dashboard" />}
+          {isLoading ? (
+            <Loading loading={isLoading} />
+          ) : isAuthenticated ? (
+            <Logout />
+          ) : (
+            <Redirect to="/" />
+          )}
         </Route>
         <Route exact path="/">
-          <Home />
+          {isLoading ? <Loading loading={isLoading} /> : <Home />}
+        </Route>
+        <Route>
+          {isLoading ? <Loading loading={isLoading} /> : <NotFound />}
         </Route>
       </Switch>
     </Router>
   );
 };
+
+const LoggedInRoutes = [
+  <Route exact path="/" component={Home} />,
+  <Route exact path="/dashboard" component={Dashboard} />,
+];
+
+const LoggedOutRoutes = [
+  <Route exact path="/login" component={Login} />,
+  <Route exact path="/register" component={Register} />,
+  <Route exact path="/" component={Home} />,
+];
 
 export default Main;
