3eb2cd9a451be383e2e8f70dc019eaea205e6726
git-shad0w <aboodshadow@outlook.com>
2021-01-18 14:06:44 +0400

    Fixed navbar bug which caused a Drawer.js Bug

--- src/Pages/Home/Navbar/LinksContainer.js
+++ src/Pages/Home/Navbar/LinksContainer.js
@@ -1,45 +1,146 @@
 /** @jsxImportSource @emotion/core */
 
 import React, { useEffect, useState } from "react";
-import Link from "./Link";
-import Button from "../../../Components/GlobalComponents/Button";
+import CustomLink from "./Link";
 import Github from "./Github";
+import { Link } from "react-router-dom";
+import Button from "@material-ui/core/Button";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  typography: {
+    fontFamily: ["Poppins", "sans-serif"].join(","),
+    fontWeight: "300",
+  },
+
+  root: {
+    display: "flex",
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+  linkLabel: {
+    color: "white",
+  },
+  btngroup: {
+    width: "100%",
+  },
+  btnLabel: {
+    justifyContent: "flex-start",
+    paddingRight: "1rem",
+    paddingLeft: "1rem",
+    color: "white",
+    backgroundColor: "rgba(0, 0, 0, 0.01)",
+    fontSize: "16px",
+    transition: "all 0.25s ease",
+    border: "0.5px solid rgba(255, 0, 0, 0)",
+    "&:focus": {
+      backgroundColor: "rgba(255,0,0 0.5)",
+      color: "red",
+      borderBottom: "0.5px solid rgba(255, 0, 0, 1)",
+    },
+    "&:hover": {
+      backgroundColor: "rgba(255,0,0, 1)",
+      color: "white",
+    },
+  },
+  firstBtnLabel: {
+    justifyContent: "flex-start",
+    paddingRight: "1rem",
+    paddingLeft: "1rem",
+    color: "white",
+    backgroundColor: "rgba(0, 0, 0, 0.15)",
+    fontSize: "16px",
+    transition: "all 0.25s ease",
+    borderRadiusTopRight: "15px",
+    borderRadiusBottomRight: "15px",
+    borderBottom: "0.5px solid rgba(255, 0, 0, 0.25)",
+    "&:focus": {
+      backgroundColor: "rgba(255,0,0 0.5)",
+      color: "red",
+      borderBottom: "0.5px solid rgba(255, 0, 0, 1)",
+    },
+    "&:hover": {
+      backgroundColor: "rgba(255,0,0, 1)",
+      color: "white",
+    },
+  },
+  lastBtnLabel: {
+    justifyContent: "flex-start",
+    paddingRight: "1rem",
+    paddingLeft: "1rem",
+    color: "white",
+    backgroundColor: "rgba(0, 0, 0, 0.15)",
+    fontSize: "16px",
+    transition: "all 0.25s ease",
+    borderRadiusTopRight: "15px",
+    borderRadiusBottomRight: "15px",
+    borderBottom: "0.5px solid rgba(255, 0, 0, 0.25)",
+    "&:focus": {
+      backgroundColor: "rgba(255,0,0 0.5)",
+      color: "red",
+      borderBottom: "0.5px solid rgba(255, 0, 0, 1)",
+    },
+    "&:hover": {
+      backgroundColor: "rgba(255,0,0, 1)",
+      color: "white",
+    },
+  },
+}));
 
 const LinksContainer = ({ hidden, isAuth }) => {
+  const classes = useStyles();
   return (
     <div className={(hidden ? "hidden" : "") + " linksContainer"}>
-      <div className="mid">
-        <Link name="HOME" linkTo="/" />
-        <Link name="INCENTIVE" linkTo="/incentive" />
-        <Link name="OUR CAUSE" linkTo="/cause" />
-        <Link name="OUR GROUPS" linkTo="/groups" />
-      </div>
-      {
-        // If the user is authenticated it will show the register and login otherwise only logout button
-        isAuth ? (
-          <div className="btn-group">
-            <Button className="left-btn" text="DASHBOARD" linkTo="/dashboard" />
-            <Button className="right-btn" text="LOGOUT" linkTo="/logout" />
-          </div>
-        ) : (
-          <div className="btn-group">
-            <Button
-              className="left-btn"
-              text="JOIN NOW"
-              linkTo="/register"
-              className="reg-btn"
-            />
-            <Button
-              className="right-btn"
-              text="LOGIN"
-              linkTo="/login"
-              className="log-btn"
-            />
-          </div>
-        )
-
-        // If the user is authenticated it will show the register and login otherwise only logout button
-      }
+      <Button
+        variant="outlined"
+        className={classes.btnLabel}
+        component={Link}
+        to="/w/home"
+      >
+        Home
+      </Button>
+      <Button
+        variant="outlined"
+        className={classes.btnLabel}
+        component={Link}
+        to="/w/incentive"
+      >
+        Incentive
+      </Button>
+      <Button
+        variant="outlined"
+        className={classes.btnLabel}
+        component={Link}
+        to="/w/cause"
+      >
+        Cause
+      </Button>
+      <Button
+        variant="outlined"
+        className={classes.btnLabel}
+        component={Link}
+        to="/w/groups"
+      >
+        Groups
+      </Button>
+      <Button
+        variant="outlined"
+        className={classes.firstBtnLabel}
+        component={Link}
+        to={isAuth ? "/u" : "/register"}
+      >
+        {isAuth ? "Home" : "Register"}
+      </Button>
+      <Button
+        variant="outlined"
+        className={classes.lastBtnLabel}
+        component={Link}
+        to={isAuth ? "/logout" : "/login"}
+      >
+        {isAuth ? "Logout" : "Login"}
+      </Button>
       <Github />
     </div>
   );
