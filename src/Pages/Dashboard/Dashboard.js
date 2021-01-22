f035d86c641a85259aa8a2674e85713542f85f85
git-shad0w <aboodshadow@outlook.com>
2021-01-22 21:48:27 +0400

    Authentication now passed as a prop to Dashboard

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -7,37 +7,54 @@ import Explore from "./Sections/Explore/Explore";
 import Profile from "./Sections/Profile/Profile";
 import Settings from "./Sections/Settings/Settings";
 import NotFound from "../NotFound/NotFound";
-import Header from "./Drawer/Header";
 
-const Dashboard = () => {
+const Dashboard = ({ isAuthenticated }) => {
   // This gets us the User Data assigned into a state
 
   const [userData, setUserData] = useState(
     JSON.parse(localStorage.getItem("userData"))
   );
 
+  const [drawerStatus, setDrawerStatus] = useState(true);
+
   return (
     <div className="dashboard-wrapper">
       <div className="dashboard-main">
         <Switch>
           <Route exact path="/u">
-            <Header />
-            <Drawer userData={userData} drawerStatus={true} />
+            <Drawer
+              userData={userData}
+              status={drawerStatus}
+              setDrawerStatus={setDrawerStatus}
+              isAuthenticated={isAuthenticated}
+            />
             <Home />
           </Route>
           <Route exact path="/u/profile/:username">
-            <Header />
-            <Drawer userData={userData} drawerStatus={true} />
+            <Drawer
+              userData={userData}
+              status={drawerStatus}
+              setDrawerStatus={setDrawerStatus}
+              isAuthenticated={isAuthenticated}
+            />
             <Profile />
           </Route>
           <Route exact path="/u/explore">
-            <Header />
-            <Drawer userData={userData} drawerStatus={true} />
+            <Drawer
+              userData={userData}
+              status={drawerStatus}
+              setDrawerStatus={setDrawerStatus}
+              isAuthenticated={isAuthenticated}
+            />
             <Explore />
           </Route>
           <Route exact path="/u/settings">
-            <Header />
-            <Drawer userData={userData} drawerStatus={true} />
+            <Drawer
+              userData={userData}
+              status={drawerStatus}
+              setDrawerStatus={setDrawerStatus}
+              isAuthenticated={isAuthenticated}
+            />
             <Settings />
           </Route>
           <Route>
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -15,6 +15,9 @@ import {
   Route,
   Redirect,
 } from "react-router-dom";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
 import { authHeader } from "../../Services/auth-header";
 import history from "../../history";
 
@@ -41,81 +44,99 @@ const Main = ({ props }) => {
     }
   }
 
-  return (
-    <Router history={history}>
-      <Switch>
-        <Route path="/u">
-          {isLoading ? (
-            <Loading loading={isLoading} />
-          ) : isAuthenticated ? (
-            <Dashboard />
-          ) : (
-            <Redirect to="/login" />
-          )}
-        </Route>
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: light)");
 
-        <Route exact path="/login">
-          {isLoading ? (
-            <Loading loading={isLoading} />
-          ) : isAuthenticated ? (
-            <Redirect to="/u" />
-          ) : (
-            <Login />
-          )}
-        </Route>
-        <Route exact path="/register">
-          {isLoading ? (
-            <Loading loading={isLoading} />
-          ) : isAuthenticated ? (
-            <Redirect to="/u" />
-          ) : (
-            <Register />
-          )}
-        </Route>
-        <Route exact path="/logout">
-          {isLoading ? (
-            <Loading loading={isLoading} />
-          ) : isAuthenticated ? (
-            <Logout />
-          ) : (
-            <Redirect to="/" />
-          )}
-        </Route>
-        <Route path="/w">
-          {isLoading ? (
-            <Loading loading={isLoading} />
-          ) : isAuthenticated ? (
-            <Redirect to="/u" />
-          ) : (
-            <Home />
-          )}
-        </Route>
-        <Route exact path="/">
-          {isLoading ? (
-            <Loading loading={isLoading} />
-          ) : isAuthenticated ? (
-            <Redirect to="/u" />
-          ) : (
-            <Redirect to="/w/home" />
-          )}
-        </Route>
-        <Route>
-          {isLoading ? <Loading loading={isLoading} /> : <NotFound />}
-        </Route>
-      </Switch>
-    </Router>
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: prefersDarkMode ? "dark" : "light",
+          primary: {
+            light: "#f2f5fa",
+            main: "#da0202",
+            dark: "#262d37",
+            // contrastText: will be calculated to contrast with palette.primary.main
+          },
+          secondary: {
+            light: "#c7c7c7",
+            main: "#b71c1c",
+            dark: "#303741",
+          },
+          // Used by `getContrastText()` to maximize the contrast between
+          // the background and the text.
+          contrastThreshold: 3,
+          // Used by the functions below to shift a color's luminance by approximately
+          // two indexes within its tonal palette.
+          // E.g., shift from Red 500 to Red 300 or Red 700.
+          tonalOffset: 0.2,
+        },
+      }),
+    [prefersDarkMode]
+  );
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Router history={history}>
+        <Switch>
+          <Route path="/u">
+            {isLoading ? (
+              <Loading loading={isLoading} />
+            ) : (
+              <Dashboard isAuthenticated={isAuthenticated} />
+            )}
+          </Route>
+
+          <Route exact path="/login">
+            {isLoading ? (
+              <Loading loading={isLoading} />
+            ) : isAuthenticated ? (
+              <Redirect to="/u" />
+            ) : (
+              <Login />
+            )}
+          </Route>
+          <Route exact path="/register">
+            {isLoading ? (
+              <Loading loading={isLoading} />
+            ) : isAuthenticated ? (
+              <Redirect to="/u" />
+            ) : (
+              <Register />
+            )}
+          </Route>
+          <Route exact path="/logout">
+            {isLoading ? (
+              <Loading loading={isLoading} />
+            ) : isAuthenticated ? (
+              <Logout />
+            ) : (
+              <Redirect to="/" />
+            )}
+          </Route>
+          <Route path="/w">
+            {isLoading ? (
+              <Loading loading={isLoading} />
+            ) : isAuthenticated ? (
+              <Redirect to="/u" />
+            ) : (
+              <Home />
+            )}
+          </Route>
+          <Route exact path="/">
+            {isLoading ? (
+              <Loading loading={isLoading} />
+            ) : isAuthenticated ? (
+              <Redirect to="/u" />
+            ) : (
+              <Redirect to="/w/home" />
+            )}
+          </Route>
+          <Route>
+            {isLoading ? <Loading loading={isLoading} /> : <NotFound />}
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 };
-
-const LoggedInRoutes = [
-  <Route exact path="/" component={Home} />,
-  <Route exact path="/u" component={Dashboard} />,
-];
-
-const LoggedOutRoutes = [
-  <Route exact path="/login" component={Login} />,
-  <Route exact path="/register" component={Register} />,
-  <Route exact path="/" component={Home} />,
-];
-
 export default Main;
