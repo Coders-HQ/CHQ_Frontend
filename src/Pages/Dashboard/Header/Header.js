fa3465fda30128814e425b64a2bedd46df96c989
git-shad0w <aboodshadow@outlook.com>
2021-01-25 21:17:23 +0400

    Header Component

---
+++ src/Pages/Dashboard/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = ({ userData, isAuthenticated }) => {
+  return (
+    <div className="dashboard-header">
+      <h1>Test</h1>
+    </div>
+  );
+};
+
+export default Header;
