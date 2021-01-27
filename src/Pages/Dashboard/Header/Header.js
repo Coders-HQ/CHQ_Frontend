f564b83e3afcc204e1df697e40b7d2f040ff37b6
git-shad0w <aboodshadow@outlook.com>
2021-01-27 15:24:58 +0400

    Added a welcome message in Dashboard Header (test)

--- src/Pages/Dashboard/Header/Header.js
+++ src/Pages/Dashboard/Header/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = ({ userData, isAuthenticated, darkMode }) => {
   return (
     <div className={"dashboard-header " + (darkMode ? "dark" : "light")}>
-      <h1>Test</h1>
+      <h1>Welcome back {userData.username}! </h1>
     </div>
   );
 };
