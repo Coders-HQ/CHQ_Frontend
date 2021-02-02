5d57d1fd0e4f3aa43bf76ecaeee99df825edc4b3
git-shad0w <aboodshadow@outlook.com>
2021-02-02 16:21:53 +0400

    Removed unused code

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -4,11 +4,7 @@ import Sidebar from "./components/Sidebar";
 import Footer from "./components/Footer";
 import HeroSection from "./components/HeroSection";
 import InfoSection from "./components/InfoSection";
-import {
-  globalHackathons,
-  homeObjTwo,
-  homeObjThree,
-} from "./components/InfoSection/Data";
+import { homeObjThree } from "./components/InfoSection/Data";
 import Services from "./components/Services";
 import FaqSection from "./components/FaqSection/faqSection";
 import SlideSection from "./components/SlideSection";
