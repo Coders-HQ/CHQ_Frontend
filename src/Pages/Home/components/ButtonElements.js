8b9e3d37d8651f6e332627fa4144f96d6f705f23
git-shad0w <aboodshadow@outlook.com>
2021-02-02 16:22:04 +0400

    Fixed register button bug

--- src/Pages/Home/components/ButtonElements.js
+++ src/Pages/Home/components/ButtonElements.js
@@ -1,7 +1,28 @@
 import styled from "styled-components";
 import { Link } from "react-scroll";
+import { Link as LinkR } from "react-router-dom";
 
 export const Button = styled(Link)`
+border-radius: 50px;
+background: #b71c1c;
+white-space: nowrap;
+padding: ${({ big }) => (big ? "14px 48px" : "12px 30px")};
+color: ${({ dark }) => (dark ? "#FFF" : "#FFF")}
+font-size: ${({ fontBig }) => (fontBig ? "20px" : "16px")}
+outline: none;
+border: none;
+cursor: pointer;
+display: flex;
+justify-content: center;
+align-items: center;
+transition: all 0.2s ease-in-out;
+
+&:hover{
+    transition: all 0.2s ease-in-out;
+    background: #da0202;
+`;
+
+export const ButtonR = styled(LinkR)`
 border-radius: 50px;
 background: #b71c1c;
 white-space: nowrap;
